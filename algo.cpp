5d88160adb9ad4c87c15fd2f96b4b36153168c9c
여동준 <tgc01099@gmail.com>
2023-09-06 16:02:05 +0900

    3. 이상한 다트 게임(TC 3번 오류)

--- algo.cpp
+++ algo.cpp
@@ -2,11 +2,6 @@
 #include <queue>
 using namespace std;
 
-/*
-    1. 회전은 독립적 -> row 부분만 회전하면 됨
-    2. 0은 시계, 1은 반시계
-*/
-
 int n, m, q, a, d, k;
 vector<vector<int>> MAP;
 
@@ -24,12 +19,44 @@ void rotate(int plate, int dir, int cnt) {
             (dir) ? swap(MAP[plate][j], MAP[plate][j + 1]) : swap(MAP[plate][m - 1 - j], MAP[plate][m - 2 - j]);
 }
 
-void erase_num() {
-    vector<vector<bool>> erase_MAP(n, vector<bool>(m, false));
-    for (int y = 0; y < n; y++) {
-        for (int x = 0; x < m; x++) {
-        }
+bool isSame(int y, int x) {
+    int dy[] = {-1, 1, 0, 0};
+    int dx[] = {0, 0, -1, 1};
+    if (!MAP[y][x])
+        return false;
+
+    for (int i = 0; i < 4; i++) {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        nx = (nx < 0) ? 3 : (nx >= m) ? 0 : nx;
+        if (0 <= ny && ny < n && MAP[ny][nx] && MAP[y][x] == MAP[ny][nx])
+            return true;
     }
+    return false;
+}
+
+void erase_num() {
+    int avg = 0;
+    bool isErase = false;
+    vector<vector<bool>> erase_MAP(n, vector<bool>(m, false));
+
+    // 1. 지울 대상 선정(인접 여부 확인)
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < m; x++) {
+            avg += MAP[y][x];
+            if (isSame(y, x))
+                erase_MAP[y][x] = isErase = true;
+        }
+
+    avg /= n * m;
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < m; x++)
+            // 2. 지우기
+            if (erase_MAP[y][x])
+                MAP[y][x] = 0;
+            // 3. 지워지는 수가 없을 때
+            else if (!isErase && MAP[y][x])
+                (MAP[y][x] > avg) ? MAP[y][x]-- : (MAP[y][x] < avg) ? MAP[y][x]++ : 1;
 }
 
 int main() {
