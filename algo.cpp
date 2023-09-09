572226d9ab1e6e369cbe2e9675c73999199507e8
ChrisYeo <tgc01099@gmail.com>
2023-09-10 01:41:52 +0900

    4. 이상한 윷놀이(codetree)

--- algo.cpp
+++ algo.cpp
@@ -1,84 +1,95 @@
+#include <algorithm>
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
 
-int n, m, q, a, d, k;
-vector<vector<int>> MAP;
+// d: 우좌상하
+// 0: 위에 쌓는다, 1: 빨간 칸에 가기 전에 순서 반전, 2: 벽(격자 바운더리와 같은 개념)
+int dy[] = {0, 0, -1, 1};
+int dx[] = {1, -1, 0, 0};
+int n, k;
+vector<vector<int>> color_MAP;
+vector<vector<vector<pair<int, int>>>> mal_MAP;
 
 void input() {
-    cin >> n >> m >> q;
-    MAP = vector<vector<int>>(n, vector<int>(m));
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < m; x++)
-            cin >> MAP[y][x];
-}
+    cin >> n >> k;
+    color_MAP = vector<vector<int>>(n, vector<int>(n));
+    mal_MAP = vector<vector<vector<pair<int, int>>>>(n, vector<vector<pair<int, int>>>(n));
+    for (int i = 0; i < n; i++)
+        for (auto &&j : color_MAP[i])
+            cin >> j;
 
-void rotate(int plate, int dir, int cnt) {
-    for (int i = 0; i < cnt; i++)
-        for (int j = 0; j < m - 1; j++)
-            (dir) ? swap(MAP[plate][j], MAP[plate][j + 1]) : swap(MAP[plate][m - 1 - j], MAP[plate][m - 2 - j]);
-}
-
-bool isSame(int y, int x) {
-    if (!MAP[y][x])
-        return false;
-
-    int dy[] = {-1, 1, 0, 0};
-    int dx[] = {0, 0, -1, 1};
-    for (int i = 0; i < 4; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        nx = (nx < 0) ? m - 1 : (nx >= m) ? 0 : nx;
-        if (0 <= ny && ny < n && MAP[y][x] == MAP[ny][nx])
-            return true;
+    for (int i = 0; i < k; i++) {
+        int y, x, d;
+        cin >> y >> x >> d;
+        mal_MAP[y - 1][x - 1].push_back({i, d - 1});
     }
+}
+
+bool isWall(int y, int x) {
+    if (y < 0 || y >= n || x < 0 || x >= n)
+        return true;
+    return color_MAP[y][x] == 2;
+}
+
+bool isOverlap() {
+    for (int i = 0; i < n; i++)
+        for (auto &&j : mal_MAP[i])
+            if (j.size() >= 4)
+                return true;
     return false;
 }
 
-vector<vector<bool>> erase_num() {
-    int avg = 0, cnt = 0;
-    bool isErase = false;
-    vector<vector<bool>> erase_MAP(n, vector<bool>(m, false));
+void mal_move(int y, int x, int i) {
+    int ny = y + dy[mal_MAP[y][x][i].second];
+    int nx = x + dx[mal_MAP[y][x][i].second];
+    vector<pair<int, int>> tmp;
+    if (isWall(ny, nx))
+        (mal_MAP[y][x][i].second % 2) ? mal_MAP[y][x][i].second-- : mal_MAP[y][x][i].second++;
 
-    // 1. ???? ??? ????(???? ???? ???)
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < m; x++) {
-            if (isSame(y, x))
-                erase_MAP[y][x] = isErase = true;
-            else if (MAP[y][x]) {
-                avg += MAP[y][x];
-                cnt++;
-            }
-        }
+    ny = y + dy[mal_MAP[y][x][i].second];
+    nx = x + dx[mal_MAP[y][x][i].second];
+    if (isWall(ny, nx))
+        return;
 
-    if (cnt)
-        avg /= cnt;
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < m; x++)
-            // 2. ?????
-            if (erase_MAP[y][x])
-                MAP[y][x] = 0;
-            // 3. ???????? ???? ???? ??
-            else if (!isErase && MAP[y][x])
-                (MAP[y][x] > avg) ? MAP[y][x]-- : (MAP[y][x] < avg) ? MAP[y][x]++ : 1;
-    return v;
+    for (int j = i; j >= 0; j--) {
+        tmp.push_back(mal_MAP[y][x][j]);
+        mal_MAP[y][x].erase(mal_MAP[y][x].begin() + j);
+    }
+
+    int sz = tmp.size();
+    for (int j = 0; j < sz; j++) {
+        pair<int, int> insert_val = (!color_MAP[ny][nx]) ? tmp[j] : tmp[sz - 1 - j];
+        mal_MAP[ny][nx].insert(mal_MAP[ny][nx].begin(), insert_val);
+    }
 }
 
 int main() {
     freopen("./input.txt", "r", stdin);
     input();
 
-    for (int i = 0; i < q; i++) {
-        cin >> a >> d >> k;
-        for (int j = a - 1; j < n; j += a)
-            rotate(j, d, k);
-        exist_MAP = erase_num();
+    int ans = 0;
+    while (!isOverlap() && ans <= 1000) {
+        ans++;
+        for (int i = 0; i < k; i++) {
+            bool flag = false;
+            for (int y = 0; y < n; y++) {
+                for (int x = 0; x < n; x++) {
+                    auto it = find_if(mal_MAP[y][x].begin(), mal_MAP[y][x].end(), [i](const pair<int, int> &element) { return element.first == i; });
+                    if (flag = it != mal_MAP[y][x].end()) {
+                        mal_move(y, x, distance(mal_MAP[y][x].begin(), it));
+                        break;
+                    }
+                }
+                if (flag)
+                    break;
+            }
+            if (isOverlap())
+                break;
+        }
     }
 
-    int ans = 0;
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < m; x++)
-            ans += MAP[y][x];
+    ans = (ans >= 1000) ? -1 : ans;
     cout << ans;
     return 0;
 }
