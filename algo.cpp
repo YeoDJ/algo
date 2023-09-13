1a74cbd6a634bb0c88dd9e93ddbd63579dad943f
ChrisYeo <tgc01099@gmail.com>
2023-09-13 15:48:40 +0900

    7. 디버깅(codetree)

--- algo.cpp
+++ algo.cpp
@@ -1,93 +1,71 @@
-#include <algorithm>
 #include <iostream>
 #include <queue>
 using namespace std;
 
-int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
-int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
-int n, m, k;
-vector<vector<int>> food, plus_food;
-vector<vector<vector<int>>> virus;
-
-bool inRange(int y, int x) { return 0 <= y && y < n && 0 <= x && x < n; }
+int n, m, h, ans = -1;
+vector<vector<bool>> MAP;
+vector<pair<int, int>> path;
 
 void input() {
-    cin >> n >> m >> k;
-    food = vector<vector<int>>(n, vector<int>(n, 5));
-    plus_food = food;
-    virus = vector<vector<vector<int>>>(n, vector<vector<int>>(n));
-
-    for (int i = 0; i < n; i++)
-        for (auto &&j : plus_food[i])
-            cin >> j;
+    cin >> n >> m >> h;
+    MAP = vector<vector<bool>>(h, vector<bool>(--n, false));
     for (int i = 0; i < m; i++) {
-        int y, x, age;
-        cin >> y >> x >> age;
-        virus[y - 1][x - 1].push_back(age);
+        int a, b;
+        cin >> a >> b;
+        MAP[a - 1][b - 1] = true;
     }
 }
 
-void sort_virus() {
-    // virus[0]이 가장 나이 많은 바이러스
-    for (int i = 0; i < n; i++)
-        for (auto &&j : virus[i])
-            if (j.size())
-                sort(j.begin(), j.end());
+bool isCorrect() {
+    vector<vector<bool>> tmp = MAP;
+    for (auto &&i : path)
+        tmp[i.first][i.second] = true;
+
+    for (int x = 0; x < n; x++) {
+        int nx = x;
+        for (int y = 0; y < h; y++)
+            (0 < nx && tmp[y][nx - 1]) ? nx-- : (nx < n && tmp[y][nx]) ? nx++ : nx;
+        if (nx != x)
+            return false;
+    }
+    return true;
 }
 
-void food_algo() {
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++) {
-            int i, sz = virus[y][x].size();
-            // 어린 바이러스부터 양분 섭취, 본인 나이만큼 먹기
-            for (i = 0; i < sz; i++)
-                if (virus[y][x][i] <= food[y][x])
-                    food[y][x] -= virus[y][x][i]++;
-                else
-                    break;
+void solution(int sy, int sx, int lvl, int max_lvl) {
+    if (lvl == max_lvl) {
+        if (isCorrect())
+            ans = lvl;
+        return;
+    }
 
-            // 죽은 바이러스 / 2만큼 양분 추가 후 소멸
-            for (int j = sz - 1; j >= i; j--) {
-                food[y][x] += virus[y][x][j] / 2;
-                virus[y][x].pop_back();
-            }
+    for (int x = sx; x < n; x++)
+        for (int y = sy; y < h; y++) {
+            if (MAP[y][x] || (x > 0 && MAP[y][x - 1]) || (x < n - 1 && MAP[y][x + 1]))
+                continue;
+
+            path.push_back({y, x});
+            solution(y, x, lvl + 1, max_lvl);
+            path.pop_back();
+            if (ans != -1)
+                return;
         }
-}
-
-void virus_algo(int y, int x) {
-    for (int i = 0; i < 8; i++) {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
-        if (inRange(ny, nx))
-            virus[ny][nx].push_back(1);
-    }
 }
 
 int main() {
     freopen("./input.txt", "r", stdin);
     input();
 
-    for (int i = 0; i < k; i++) {
-        sort_virus();
-        food_algo();
-
-        // 번식
-        for (int y = 0; y < n; y++)
-            for (int x = 0; x < n; x++)
-                for (int i = 0; i < virus[y][x].size(); i++)
-                    if (!(virus[y][x][i] % 5))
-                        virus_algo(y, x);
-
-        // 양분 추가
-        for (int y = 0; y < n; y++)
-            for (int x = 0; x < n; x++)
-                food[y][x] += plus_food[y][x];
+    if (isCorrect()) {
+        cout << 0;
+        return 0;
     }
 
-    int ans = 0;
-    for (int i = 0; i < n; i++)
-        for (auto &&j : virus[i])
-            ans += j.size();
+    for (int i = 1; i <= 3; i++) {
+        solution(0, 0, 0, i);
+        if (ans != -1)
+            break;
+    }
+
     cout << ans;
     return 0;
 }
