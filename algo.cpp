2efbe21e6c01ebb6d5f645613f53049ec324fd69
ChrisYeo <tgc03042@gmail.com>
2023-09-28 11:58:18 +0900

    shit

--- algo.cpp
+++ algo.cpp
@@ -2,19 +2,43 @@
 #include <queue>
 using namespace std;
 
-int n, m, q, x, d, k;
-vector<vector<int>> MAP;
+int n, k;
 
-void input() {
-    cin >> n >> m >> q;
-    MAP = vector<vector<int>>(n, vector<int>(4));
-    for (int y = 0; y < n; y++)
-        for (int x = 0; x < n; x++)
-            cin >> MAP[y][x];
-    cin >> x >> d >> k;
+vector<vector<char>> MAP;
+
+bool input() {
+    cin >> n;
+    if (n < 1 || n > 100 || !(n % 2)) {
+        cout << "INPUT ERROR!";
+        return false;
+    }
+    k = n / 2 + 1;
+    MAP = vector<vector<char>>(n, vector<char>(n, ' '));
+    return true;
+}
+
+void solution() {
+    int cnt = 1;
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < cnt; j++)
+            MAP[i][j] = MAP[n - 1 - i][j] = '*';
+        cnt += 2;
+    }
 }
 
 int main() {
-    input();
+    if (!input())
+        return 0;
+
+    solution();
+    for (int i = 0; i < n; i++) {
+        int cnt = (i < k) ? i : n - 1 - i;
+        for (int j = 0; j < cnt; j++)
+            cout << ' ';
+        for (auto &&i : MAP[i])
+            cout << i;
+        cout << endl;
+    }
+
     return 0;
 }
