f01c404299f027b8689d25b52e84b5d47e3a9a2f
ChrisYeo <tgc03042@gmail.com>
2023-10-03 13:36:17 +0900

    11. 색깔 폭탄(dev)

--- algo.cpp
+++ algo.cpp
@@ -1,144 +1,140 @@
 #include <algorithm>
 #include <iostream>
 #include <queue>
+#include <unordered_set>
 using namespace std;
 
-struct player {
-    int y, x, dir, stat;
-    int gun, score;
-};
-
-int dy[] = {-1, 0, 1, 0};
-int dx[] = {0, 1, 0, -1};
-int n, m, k;
-vector<vector<vector<int>>> MAP;
-vector<player> arr;
+// -1: 흑돌, 0: 빨폭, 1 ~ m: 그 이외
+// red_cnt: 묶음 중 빨간폭탄 개수, std_p: 기준점
+int n, m;
+int red_cnt = 0, tmp_red_cnt;
+pair<int, int> std_p, tmp_std_p;
+vector<vector<int>> MAP, used, bundle, tmp_bundle;
+unordered_set<pair<int, int>> red_bomb;
 
 bool inRange(int y, int x) { return 0 <= y && y < n && 0 <= x && x < n; }
 
 void input() {
-    cin >> n >> m >> k;
-    MAP = vector<vector<vector<int>>>(n, vector<vector<int>>(n));
-    arr = vector<player>(m);
-
-    for (int i = 0; i < n; i++)
-        for (auto &&j : MAP[i]) {
-            int g;
-            cin >> g;
-            if (g)
-                j.push_back(g);
+    cin >> n >> m;
+    MAP = vector<vector<int>>(n, vector<int>(n));
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++) {
+            cin >> MAP[y][x];
+            if (MAP[y][x] == 0)
+                red_bomb.insert({y, x});
         }
-    for (auto &&i : arr) {
-        cin >> i.y >> i.x >> i.dir >> i.stat;
-        i.y--, i.x--;
-        i.score = i.gun = 0;
-    }
 }
 
-int isTwoPlayer(player p, int idx) {
-    for (int i = 0; i < m; i++)
-        if (i != idx && arr[i].y == p.y && arr[i].x == p.x)
-            return i;
-    return -1;
-}
+// 묶음 크기
+int bfs(int ny, int nx) {
+    tmp_bundle = vector<vector<int>>(n, vector<int>(n, 0));
+    tmp_red_cnt = 0;
+    tmp_std_p = {-1, n};
+    int dy[] = {-1, 1, 0, 0};
+    int dx[] = {0, 0, -1, 1};
+    int cnt = 0;
 
-void move_player(player &p, bool isFight, int idx) {
-    player tmp = p;
-    tmp.y = p.y + dy[p.dir];
-    tmp.x = p.x + dx[p.dir];
+    queue<pair<int, int>> q;
+    q.push({ny, nx});
+    used[ny][nx] = 1;
 
-    if (!isFight && !inRange(tmp.y, tmp.x))
-        p.dir += (p.dir >= 2) ? -2 : 2;
-    else if (isFight)
+    while (!q.empty()) {
+        pair<int, int> cur = q.front();
+        tmp_bundle[cur.first][cur.second] = 1;
+        tmp_red_cnt += (red_bomb.find(cur) != red_bomb.end()) ? 1 : 0;
+        tmp_std_p = (MAP[cur.first][cur.second] > 0 && (cur.first > tmp_std_p.first || (cur.first == tmp_std_p.first && cur.second < tmp_std_p.second))) ? cur : tmp_std_p;
+        q.pop();
+
         for (int i = 0; i < 4; i++) {
-            if (inRange(tmp.y, tmp.x) && isTwoPlayer(tmp, idx) < 0)
-                break;
-            p.dir = (p.dir == 3) ? 0 : p.dir + 1;
-            tmp.y = p.y + dy[p.dir];
-            tmp.x = p.x + dx[p.dir];
+            int y = ny + dy[i];
+            int x = nx + dx[i];
+            if (inRange(y, x) && !used[y][x] && (MAP[y][x] == MAP[ny][nx] || MAP[y][x] == 0)) {
+                used[y][x] = 1;
+                cnt++;
+                q.push({y, x});
+            }
         }
-    p.y += dy[p.dir], p.x += dx[p.dir];
+    }
+    return cnt;
 }
 
-void get_gun(player &p) {
-    if (p.gun)
-        MAP[p.y][p.x].push_back(p.gun);
-    sort(MAP[p.y][p.x].begin(), MAP[p.y][p.x].end());
-    p.gun = MAP[p.y][p.x][MAP[p.y][p.x].size() - 1];
-    MAP[p.y][p.x].pop_back();
+bool compare(int sz1, int sz2) {
+    if (sz1 == sz2) {
+        if (tmp_red_cnt == red_cnt) {
+            if (tmp_std_p.first == std_p.first)
+                return tmp_std_p.second < std_p.second;
+            return tmp_std_p.first > std_p.first;
+        }
+        return tmp_red_cnt < red_cnt;
+    }
+    return sz1 > sz2;
 }
 
-// p1이면 true, p2이면 false
-bool isWinner(player p1, player p2) {
-    int s1 = p1.stat + p1.gun, s2 = p2.stat + p2.gun;
-    if (s1 == s2)
-        return p1.stat > p2.stat;
-    return s1 > s2;
+bool isBomb() {
+    int max_sz = 0;
+    used = vector<vector<int>>(n, vector<int>(n, 0));
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++)
+            if (MAP[y][x] == -1)
+                used[y][x] = 1;
+
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++)
+            if (!used[y][x] && MAP[y][x] > 0) {
+                int sz = bfs(y, x);
+                if (compare(sz, max_sz)) {
+                    max_sz = sz;
+                    red_cnt = tmp_red_cnt;
+                    std_p = tmp_std_p;
+                    bundle = tmp_bundle;
+                }
+                for (auto &&i : red_bomb)
+                    used[i.first][i.second] = 0;
+            }
+    return max_sz > 1;
 }
 
-void fight_player(player &p1, player &p2, int idx) {
-    int score = abs(p1.stat + p1.gun - (p2.stat + p2.gun));
-    if (isWinner(p1, p2)) {
-        // 진 플레이어
-        MAP[p2.y][p2.x].push_back(p2.gun);
-        p2.gun = 0;
-        move_player(p2, true, idx);
-        if (MAP[p2.y][p2.x].size())
-            get_gun(p2);
+int bomb() {
+    // 폭 ★ 발
+    int cnt = 0;
+    for (int y = 0; y < n; y++)
+        for (int x = 0; x < n; x++)
+            if (bundle[y][x]) {
+                cnt++;
+                MAP[y][x] = 0;
+            }
+    return cnt * cnt;
+}
 
-        // 이긴 플레이어
-        p1.score += score;
-        get_gun(p1);
-    } else {
-        // 진 플레이어
-        MAP[p1.y][p1.x].push_back(p1.gun);
-        p1.gun = 0;
-        move_player(p1, true, idx);
-        if (MAP[p1.y][p1.x].size())
-            get_gun(p1);
-
-        // 이긴 플레이어
-        p2.score += score;
-        get_gun(p2);
+void drop() {
+    // 중 ↓↓ 력
+    vector<vector<int>> tmp_MAP = MAP;
+    for (int x = 0; x < n; x++) {
+        int ny = n - 1;
+        for (int y = n - 1; y >= 0; y--) {
+            if (MAP[y][x] > 0) {
+                tmp_MAP[ny][x] = MAP[y][x];
+                ny--;
+            } else if (MAP[y][x] == -1)
+                ny = y - 1;
+        }
     }
 }
 
-void debug() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (!MAP[i][j].size())
-                cout << 0;
-            else
-                for (auto &&k : MAP[i][j])
-                    cout << k;
-            cout << ' ';
-        }
-        cout << endl;
-    }
-
-    cout << endl;
-    for (auto &&i : arr)
-        cout << i.y << ' ' << i.x << ' ' << i.dir << ' ' << i.stat << ' ' << i.gun << endl;
-    cout << endl;
-}
+void rotate() {}
 
 int main() {
     freopen("./input.txt", "r", stdin);
     input();
+    int ans = 0;
 
-    for (int i = 0; i < k; i++) {
-        for (int j = 0; j < m; j++) {
-            move_player(arr[j], false, j);
-            int idx = isTwoPlayer(arr[j], j);
-            if (MAP[arr[j].y][arr[j].x].size() && idx < 0)
-                get_gun(arr[j]);
-            else if (idx >= 0)
-                fight_player(arr[j], arr[idx], idx);
-            debug();
-        }
+    while (isBomb()) {
+        ans += bomb();
+        drop();
+        rotate();
+        drop();
     }
 
-    for (auto &&i : arr)
-        cout << i.score << ' ';
+    cout << ans;
     return 0;
 }
