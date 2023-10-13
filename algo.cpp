56be2c3e73b1b6df6d6958044942bbc682f5c61a
ChrisYeo <tgc03042@gmail.com>
2023-10-13 23:32:01 +0900

    20. 산타의 선물 공장2(dev-vector버전)

--- algo.cpp
+++ algo.cpp
@@ -1,32 +1,22 @@
 #include <algorithm>
 #include <iostream>
-#include <list>
+#include <queue>
 #include <unordered_map>
 using namespace std;
 
-struct PairHash {
-    template <class T1, class T2> size_t operator()(const pair<T1, T2> &p) const {
-        auto h1 = hash<T1>{}(p.first);
-        auto h2 = hash<T2>{}(p.second);
-        return h1 ^ h2;
-    }
-};
-
 int n, m;
-vector<list<int>> MAP;
+vector<vector<int>> MAP;
 // 해당 box 번호가 어느 벨트에 있는지 저장
-unordered_map<int, pair<int, int>> box_to_belt;
-unordered_map<pair<int, int>, int, PairHash> belt_to_box;
+unordered_map<int, pair<int, int>> box_to_pos;
 
 void input() {
     cin >> n >> m;
-    MAP = vector<list<int>>(n, list<int>());
+    MAP = vector<vector<int>>(n, vector<int>());
     for (int i = 0; i < m; i++) {
         int num;
         cin >> num;
         MAP[num - 1].push_back(i + 1);
-        box_to_belt[i + 1] = {num - 1, MAP[num - 1].size() - 1};
-        belt_to_box[box_to_belt[i + 1]] = i + 1;
+        box_to_pos[i + 1] = {num - 1, MAP[num - 1].size() - 1};
     }
 }
 
@@ -35,112 +25,83 @@ int move_to_all(int src, int dst) {
     if (!sz)
         return MAP[dst].size();
 
-    // 원래 dst에 있는 선물 상자의 box_to_belt 업데이트
-    for (auto &&i : MAP[dst]) {
-        box_to_belt[i].second += sz;
-        belt_to_box[box_to_belt[i]] = i;
-    }
     // 선물 상자 옮기기
-    for (int i = sz - 1; i >= 0; i--) {
-        int val = MAP[src].back();
-        // box_to_belt 업데이트
-        box_to_belt[val] = {dst, i};
-        belt_to_box[box_to_belt[val]] = val;
-        MAP[dst].push_front(val);
+    for (int i = 0; i < sz; i++) {
+        MAP[dst].insert(MAP[dst].begin(), MAP[src][MAP[src].size() - 1]);
         MAP[src].pop_back();
     }
+
+    // box_to_pos 업데이트
+    for (int i = 0; i < MAP[dst].size(); i++)
+        box_to_pos[MAP[dst][i]] = {dst, i};
     return MAP[dst].size();
 }
 
 int change_to_front(int src, int dst) {
-    int ssz = MAP[src].size(), dsz = MAP[dst].size(), val, val2;
+    int ssz = MAP[src].size(), dsz = MAP[dst].size();
     if (!ssz && !dsz)
-        return dsz;
+        return MAP[dst].size();
 
-    // box_to_belt 업데이트 후 선물 상자 옮기기
+    // 선물 상자를 먼저 옮긴 뒤 box_to_pos 업데이트
     if (!ssz) {
-        for (auto &&i : MAP[dst]) {
-            box_to_belt[i].second--;
-            belt_to_box[box_to_belt[i]] = i;
-        }
-        val = MAP[dst].front();
-        MAP[src].push_front(val);
-        MAP[dst].pop_front();
-        box_to_belt[val] = {src, 0};
-        belt_to_box[box_to_belt[val]] = val;
+        MAP[src].push_back(MAP[dst][0]);
+        MAP[dst].erase(MAP[dst].begin());
+        box_to_pos[MAP[src][0]] = {src, 0};
+        for (int i = 0; i < dsz - 1; i++)
+            box_to_pos[MAP[dst][i]] = {dst, i};
         dsz--;
     } else if (!dsz) {
-        for (auto &&i : MAP[src]) {
-            box_to_belt[i].second--;
-            belt_to_box[box_to_belt[i]] = i;
-        }
-        val = MAP[src].front();
-        MAP[dst].push_front(val);
-        MAP[src].pop_front();
-        box_to_belt[val] = {dst, 0};
-        belt_to_box[box_to_belt[val]] = val;
+        MAP[dst].push_back(MAP[src][0]);
+        MAP[src].erase(MAP[src].begin());
+        box_to_pos[MAP[dst][0]] = {dst, 0};
+        for (int i = 0; i < ssz - 1; i++)
+            box_to_pos[MAP[src][i]] = {src, i};
         dsz++;
     } else {
-        val2 = MAP[src].front();
-        val = MAP[dst].front();
-        MAP[src].pop_front();
-        MAP[dst].pop_front();
-        MAP[src].push_front(val);
-        MAP[dst].push_front(val2);
-        box_to_belt[val] = {src, 0};
-        belt_to_box[box_to_belt[val]] = val;
-        box_to_belt[val2] = {dst, 0};
-        belt_to_box[box_to_belt[val2]] = val2;
+        swap(MAP[src][0], MAP[dst][0]);
+        box_to_pos[MAP[src][0]] = {src, 0};
+        box_to_pos[MAP[dst][0]] = {dst, 0};
     }
     return dsz;
 }
 
 int divide_present(int src, int dst) {
-    int sz = MAP[src].size() / 2, val;
+    int sz = MAP[src].size() / 2;
     if (!sz)
         return MAP[dst].size();
 
-    // 원래 dst에 있는 선물 상자의 box_to_belt 업데이트
-    for (auto &&i : MAP[dst]) {
-        box_to_belt[i].second += sz;
-        belt_to_box[box_to_belt[i]] = i;
-    }
     // 선물 상자 옮기기
-    list<int> tmp;
-    for (int i = 0; i < sz; i++) {
-        tmp.push_back(MAP[src].front());
-        MAP[src].pop_front();
-    }
     for (int i = sz - 1; i >= 0; i--) {
-        val = tmp.back();
-        MAP[dst].push_front(val);
-        box_to_belt[val] = {dst, i};
-        belt_to_box[box_to_belt[val]] = val;
-        tmp.pop_back();
+        MAP[dst].insert(MAP[dst].begin(), MAP[src][i]);
+        MAP[src].erase(MAP[src].begin() + i);
     }
-    for (auto &&i : MAP[src]) {
-        box_to_belt[i].second -= sz;
-        belt_to_box[box_to_belt[i]] = i;
-    }
+
+    // box_to_pos 업데이트(src)
+    for (int i = 0; i < MAP[src].size(); i++)
+        box_to_pos[MAP[src][i]] = {src, i};
+    // box_to_pos 업데이트(dst)
+    for (int i = 0; i < MAP[dst].size(); i++)
+        box_to_pos[MAP[dst][i]] = {dst, i};
     return MAP[dst].size();
 }
 
 int get_present(int num) {
-    pair<int, int> p = box_to_belt[num];
-    int a = (p.second - 1 < 0) ? -1 : belt_to_box[make_pair(p.first, p.second - 1)];
-    int b = (p.second + 1 >= MAP[p.first].size()) ? -1 : belt_to_box[make_pair(p.first, p.second + 1)];
+    pair<int, int> p = box_to_pos[num];
+    int a = (p.second - 1 < 0) ? -1 : MAP[p.first][p.second - 1];
+    int b = (p.second + 1 >= MAP[p.first].size()) ? -1 : MAP[p.first][p.second + 1];
     return a + 2 * b;
 }
 
 int get_belt(int num) {
-    int c = MAP[--num].size();
-    int a = (!c) ? -1 : MAP[num].front();
-    int b = (!c) ? -1 : MAP[num].back();
+    vector<int> arr = MAP[--num];
+    int c = arr.size();
+    int a = (!c) ? -1 : arr[0];
+    int b = (!c) ? -1 : arr[c - 1];
     return a + 2 * b + 3 * c;
 }
 
 int main() {
-    freopen("./input.txt", "r", stdin);
+    // freopen("./input.txt", "r", stdin);
     int q;
     cin >> q;
 
