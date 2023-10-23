9b406d0283dae5962f2ccc9bb31c705dba1e3b59
ChrisYeo <tgc03042@gmail.com>
2023-10-23 16:25:30 +0900

    23. 루돌프의 반란(dev)

--- algo.cpp
+++ algo.cpp
@@ -1,140 +1,171 @@
-#include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <map>
 #include <queue>
-#include <stack>
-#include <string>
 #include <unordered_map>
-#include <unordered_set>
+#define DONT_MIN                                                                                                                                                                                       \
+    if (min_len == INT32_MAX)                                                                                                                                                                          \
+    return
 using namespace std;
 
-// 문제 주소, 문제 id, 채점기 id
-// 대기 시작 시간: wt 초가 되었을 때 우선순위 따져서 채점 시작
-// 채점 시작 시간, 채점 끝 시간, 우선 순위
-struct problem {
-    string domain, pid;
-    int id, wt, st, et, prior;
-};
-struct compare {
-    bool operator()(problem p1, problem p2) {
-        if (p1.prior != p2.prior)
-            return p1.prior > p2.prior;
-        return p1.wt > p2.wt;
-    }
-};
+int dy[] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+int n, m, p, c, d;
+pair<int, int> deer;            // 루돌프 위치
+vector<vector<int>> MAP;        // 지도
+map<int, pair<int, int>> santa; // 산타의 위치
+unordered_map<int, int> stun;   // 산타 스턴 시간(기본 2초)
+unordered_map<int, int> score;  // 산타 점수
 
-// 현재 Jid를 사용할 수 있는가?
-priority_queue<int, vector<int>, greater<int>> jid_arr;
-// 300 명령어(채점 시작, (domain, pq<problem>))
-unordered_map<string, priority_queue<problem, vector<problem>, compare>> waiting;
-// (domain, stack<problem>)
-unordered_map<string, stack<problem>> history;
-// 300 명령어(도메인만 모아 둠)
-unordered_set<string> j_domain, waiting_url;
-// 400 명령어(채점 종료)
-unordered_map<int, problem> judging;
+bool inRange(int y, int x) { return 0 <= y && y < n && 0 <= x && x < n; }
+
+int find_len(pair<int, int> p1, pair<int, int> p2) { return pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2); }
+
+bool compare(pair<int, int> p1, pair<int, int> p2) {
+    int len1 = find_len(deer, p1);
+    int len2 = find_len(deer, p2);
+    if (len1 != len2)
+        return len1 < len2;
+    if (p1.first != p2.first)
+        return p1.first > p2.first;
+    return p1.second > p2.second;
+}
+
+void update() {
+    MAP = vector<vector<int>>(n, vector<int>(n, 0));
+    MAP[deer.first][deer.second] = -1;
+    for (auto &&i : santa)
+        MAP[i.second.first][i.second.second] = i.first;
+}
 
 void input() {
-    int n;
-    string url;
-    cin >> n >> url;
-    for (int i = 0; i < n; i++)
-        jid_arr.push(i + 1);
-
-    int pos = url.find('/');
-    string domain = url.substr(0, pos);
-    waiting[domain].push({domain, url.substr(pos + 1), 0, 0, 0, 0, 1});
-    waiting_url.insert(url);
+    cin >> n >> m >> p >> c >> d;
+    cin >> deer.first >> deer.second;
+    --deer.first, --deer.second;
+    for (int i = 0; i < p; i++) {
+        int pn, y, x;
+        cin >> pn >> y >> x;
+        santa[pn] = {--y, --x};
+        stun[pn] = score[pn] = 0;
+    }
+    update();
 }
 
-void request_ans() {
-    int t, p;
-    string u;
-    cin >> t >> p >> u;
+void interaction(pair<int, int> cur_p, int pn, int dir) {
 
-    // 채점 대기열 못넣는 경우
-    if (waiting_url.find(u) != waiting_url.end())
-        return;
-    int pos = u.find('/');
-    string domain = u.substr(0, pos);
-    waiting[domain].push({domain, u.substr(pos + 1), 0, t, 0, 0, p});
-    waiting_url.insert(u);
 }
 
-void try_ans(int t, int id) {
-    problem p;
-    int min_prior = INT32_MAX;
-    int min_wt = INT32_MAX;
-    string domain = "";
+bool hit(pair<int, int> next_p, int pn, int &dir, int power) {
+    deer = next_p;
+    score[pn] += power;
+    dir += (dir <= 3) ? 4 : -4;
+    int y = next_p.first + dy[dir] * power;
+    int x = next_p.second + dx[dir] * power;
 
-    for (auto &&i : waiting) {
-        p = i.second.top();
-
-        // 절대 채점이 안되는 경우
-        if (j_domain.find(p.domain) != j_domain.end())
-            continue;
-        if (history.find(p.domain) != history.end()) {
-            int start = history[p.domain].top().st;
-            int gap = history[p.domain].top().et - start;
-            if (t < start + 3 * gap)
-                continue;
-        }
-        // 우선순위 확인
-        if (p.prior < min_prior || p.prior == min_prior && p.wt < min_wt) {
-            min_prior = p.prior;
-            min_wt = p.wt;
-            domain = p.domain;
-        }
+    // 산타가 격자 밖으로 나간다면 MAP에서 제외된다.
+    if (!inRange(y, x)) {
+        santa.erase(pn), stun.erase(pn);
+        return false;
     }
 
-    // 채점 시작하기 전에 우선 순위, 대기 시간 초기화
-    // 그리고 시작 시간 및 채점기 추가
-    if (min_prior != INT32_MAX) {
-        jid_arr.pop();
-        p = waiting[domain].top();
-        waiting[domain].pop();
-        waiting_url.erase(p.domain + '/' + p.pid);
-        if (waiting[domain].empty())
-            waiting.erase(domain);
-        p.wt = 0, p.prior = 0, p.st = t, p.id = id;
-        judging[p.id] = p, j_domain.insert(p.domain);
-    }
+    // 충돌한 산타는 이동하면서 스턴이 걸린다.
+    santa[pn] = {y, x}, stun[pn] = 2;
+    return MAP[y][x];
 }
 
-void ans_end() {
-    int t, id;
-    cin >> t >> id;
-    if (judging.find(id) == judging.end())
-        return;
+void move_deer() {
+    int y, x, dir;
+    int min_len = INT32_MAX, len, target = -1;
+    pair<int, int> next_deer = deer;
 
-    problem p = judging[id];
-    p.et = t, history[p.domain].push(p);
-    judging.erase(id), j_domain.erase(p.domain);
-    jid_arr.push(id);
+    // 산타와 가장 가까운 거리 구하기
+    for (auto &&i : santa) {
+        len = find_len(deer, i.second);
+        if (len <= min_len) {
+            min_len = len;
+            if (target == -1 || compare(i.second, santa[target]))
+                target = i.first;
+        }
+    }
+    DONT_MIN;
+
+    // 루돌프 이동
+    min_len = INT32_MAX;
+    for (int i = 0; i < 8; i++) {
+        y = deer.first + dy[i], x = deer.second + dx[i];
+        len = find_len({y, x}, santa[target]);
+        if (inRange(y, x) && len < min_len) {
+            min_len = len;
+            next_deer = {y, x};
+            dir = i;
+        }
+    }
+    DONT_MIN;
+
+    // 충돌하는가?
+    if (next_deer == santa[target] && hit(next_deer, target, dir, c))
+        interaction(next_deer, target, dir);
+    else if (next_deer != deer && next_deer != deer)
+        deer = next_deer;
+    update();
+}
+
+void move_santa(int pn) {
+    int y, x, dir;
+    int min_len = INT32_MAX, len;
+    pair<int, int> next_santa = santa[pn];
+
+    // 산타 이동하기
+    for (int i = 0; i < 8; i += 2) {
+        y = santa[pn].first + dy[i];
+        x = santa[pn].second + dx[i];
+        len = find_len({y, x}, deer);
+        if (inRange(y, x) && MAP[y][x] <= 0 && len < min_len) {
+            min_len = len;
+            next_santa = {y, x};
+            dir = i;
+        }
+    }
+    DONT_MIN;
+
+    // 충돌하는가?
+    if (next_santa == deer && hit(next_santa, pn, dir, d))
+        interaction(next_santa, pn, dir);
+    else if (next_santa != deer && next_santa != santa[pn])
+        santa[pn] = next_santa;
+    update();
+}
+
+void debug() {
+    for (int i = 0; i < n; i++) {
+        for (auto &&j : MAP[i])
+            cout << j << ' ';
+        cout << endl;
+    }
+    cout << endl;
 }
 
 int main() {
     freopen("./input.txt", "r", stdin);
-    int q;
-    cin >> q;
+    input();
 
-    for (int i = 0; i < q; i++) {
-        int cmd, t;
-        cin >> cmd;
-        if (cmd == 100)
-            input();
-        else if (cmd == 200)
-            request_ans();
-        else if (cmd == 300) {
-            cin >> t;
-            if (!jid_arr.empty() && !waiting.empty())
-                try_ans(t, jid_arr.top());
-        } else if (cmd == 400)
-            ans_end();
-        else if (cmd == 500) {
-            cin >> t;
-            cout << waiting_url.size() << endl;
-        }
+    while (m-- && !santa.empty()) {
+        // 루돌프와 산타 움직이기
+        move_deer();
+        for (auto &&i : santa)
+            if (!stun[distance(santa.begin(), find(santa.begin(), santa.end(), i))])
+                move_santa(i.first);
+        debug();
+        // stun 지속시간 업데이트
+        for (auto &&i : stun)
+            i.second--;
+        // 점수 업데이트
+        for (auto &&i : score)
+            if (santa.find(i.first) != santa.end())
+                i.second++;
     }
 
+    for (auto &&i : score)
+        cout << i.second << ' ';
     return 0;
 }
